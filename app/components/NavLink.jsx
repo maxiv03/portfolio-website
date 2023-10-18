ab9bea6ae24ca8404f9b16df398958f81748ad5f
maxiv03 <nocqponer04@gmail.com>
2023-10-18 05:42:47 -0300

    fix on NavLink.jsx

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -4,7 +4,7 @@ const NavLink = ({href, title}) => {
   return (
     <Link 
       href={href}
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl md:p-0 active/hover:text-transparent active/hover:bg-clip-text active/hover:bg-gradient-to-br from-FIRST_COLOR via-SECOND_COLOR to-THIRD_COLOR active/hover:border-b active/hover:border-FIRST_COLOR"
+      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl md:p-0 active:text-transparent hover:text-transparent active:bg-clip-text hover:bg-clip-text active:bg-gradient-to-br hover:bg-gradient-to-br from-FIRST_COLOR via-SECOND_COLOR to-THIRD_COLOR active:border-b hover:border-b active:border-FIRST_COLOR hover:border-FIRST_COLOR"
     >
       {title}
     </Link>

4173a5bee6979766a483fb5b1002df20aef2eac0
maxiv03 <nocqponer04@gmail.com>
2023-11-07 05:02:46 -0300

    update on EmailSection and Footer

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,18 +1,23 @@
-import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
+export async function POST(req, res) {
+  const {email, subject, message} = await req.json();
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: 'Max <maximiliano.vega333@gmail.com>',
-      to: ['maximiliano.vega333@gmail.com'],
-      subject: 'Hello world',
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
       react:( 
         <>
-          <p>Email Body</p>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting me!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
         </>
       ),
     });
--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import GithubIcon from "../../public/github-icon.svg";
 import LinkedinIcon from "../../public/linkedin-icon.svg";
@@ -5,10 +6,43 @@ import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    }
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/send";
+
+    const options = {
+      method: "POST",
+      
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSONdata,
+    };
+
+    const response = await fetch(endpoint, options);
+    const resData = await response.json();
+
+    if(response.status === 200) {
+      console.log("Message sent.");
+    }
+  };
+
   return (
-    <section className="grid md:grid-cols-2 my-4 py-24 gap-4">
+    <section className="grid md:grid-cols-2 my-4 py-24 gap-4 relative">
       <div>
-        <h5 className="text-xl font-bold text-white mb-2">Let&apos;s Connect</h5>
+        <h3 className="text-3xl xl:text-5xl font-bold mb-2">
+          <span className="text-transparent bg-clip-text bg-gradient-to-br from-FIRST_COLOR via-SECOND_COLOR to-THIRD_COLOR">
+            Let&apos;s Connect
+          </span>
+        </h3>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
           I&apos;m currently looking for new opportunities, my inbox is always open. 
@@ -31,7 +65,7 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <form className="flex flex-col">
+        <form className="flex flex-col" onSubmit={handleSubmit}>
           <div className="mb-6">
             <label 
               htmlFor="email" 
@@ -40,10 +74,11 @@ const EmailSection = () => {
               Your email
             </label>
             <input
+              name="email"
               type="email" 
               id="email" 
               required 
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              className="bg-[#181818] border border-THIRD_COLOR placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
               placeholder="example@example.com"
             />
           </div>
@@ -55,10 +90,11 @@ const EmailSection = () => {
               Subject
             </label>
             <input
+              name="subject"
               type="text" 
               id="subject" 
               required 
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              className="bg-[#181818] border border-THIRD_COLOR placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
               placeholder="Just saying hi"
             />
           </div>
@@ -72,13 +108,18 @@ const EmailSection = () => {
             <textarea
               name="message"
               id="message"
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              className="bg-[#181818] border border-THIRD_COLOR placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
               placeholder="Let's talk about..."
-            >
-
-            </textarea>
+            />
           </div>
-
+          <button
+            type="submit"
+            className="px-1 py-1 w-full rounded-lg bg-gradient-to-b from-FIRST_COLOR via-SECOND_COLOR to-THIRD_COLOR hover:drop-shadow-[0_1px_15px_rgba(176,108,169,0.80)] active:drop-shadow-[0_1px_15px_rgba(176,108,169,0.80)] text-white"
+          >
+            <span className="block bg-[#121212] rounded-lg px-5 py-2">
+              Send Message
+            </span>
+          </button>
         </form>
       </div>
     </section>
